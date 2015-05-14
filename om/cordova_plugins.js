0e76b5ef22fde58b27a62d05936dda95fff5fcaf
weiweiw3 <weiweiw3@gmail.com>
2015-05-14 17:50:27 +0800

    'x'

--- om/cordova_plugins.js
+++ om/cordova_plugins.js
@@ -1,21 +1,21 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "file": "plugin/com.ionic.keyboard/www/keyboard.js",
         "id": "com.ionic.keyboard.keyboard",
         "clobbers": [
             "cordova.plugins.Keyboard"
         ]
     },
     {
-        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "file": "plugin/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
         "id": "com.phonegap.plugins.PushPlugin.PushNotification",
         "clobbers": [
             "PushNotification"
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "file": "plugin/org.apache.cordova.device/www/device.js",
         "id": "org.apache.cordova.device.device",
         "clobbers": [
             "device"
